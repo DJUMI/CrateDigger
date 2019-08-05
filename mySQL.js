ebe8df95b43ef04e68e766f31f5325d67940b739
DJUMI <43800602+DJUMI@users.noreply.github.com>
2019-08-05 16:04:59 -0500

    Add files via upload

---
+++ mySQL.js
@@ -0,0 +1,67 @@
+export const getArtistName = `
+TODO
+`;
+
+export const getAlbumName = `
+TODO
+`;
+
+export const getLabelName = `
+TODO
+`;
+
+export const getReleaseYear = `
+TODO
+`;
+
+export const getFormat = `
+TODO
+`;
+
+export const getPrice = `
+TODO
+`;
+
+export const getAlbumArt = `
+TODO
+`;
+
+export const addToCart = `
+TODO
+`;
+
+export const getNumInCart = `
+TODO
+`;
+
+export const removeFromCart = `
+TODO
+`;
+
+export const getSearchResults = `
+TODO
+`;
+
+export const getNewListings = `
+TODO
+`;
+
+export const getHotListings = `
+TODO: Later
+`;
+
+export const getRandomListing = `
+TODO
+`;
+
+export const getRecommendedListing = `
+TODO
+`;
+
+export const getArtistSearchResults = `
+TODO
+`;
+
+export const getLabelSearchResults = `
+TODO
+`;
