17f2b075d7613d6f0cdae18f8b3494203a62fb7c
HoneyPecanPie <44027796+HoneyPecanPie@users.noreply.github.com>
2019-11-14 22:56:50 -0600

    Update SearchScreen.js

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,83 +1,138 @@
 import React from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-    TextInput,
-    TouchableOpacity,
-    KeyboardAvoidingView
+  StyleSheet, 
+  View,
+  Button
 } from 'react-native';
-
-import { SearchBar } from 'react-native-elements';
-import { ScrollView } from 'react-native-gesture-handler';
 import SearchResultsList from '../components/SearchResultsList';
-
-let darkBlue = '#0b121c';
-let nearWhite = '#fafafa';
-
+import { SearchBar } from 'react-native-elements';
+import Drawer from 'react-native-drawer'
+import FilterScreen from './FilterScreen';
 export default class SearchScreen extends React.Component {
-    constructor(props) {
+  constructor(props) {
         super(props);
         this.state = {
-            currentUserId: undefined,
-            client: undefined,
-            tasks: undefined,
-            refreshing: false,
-            search: "",
-            cart: global.cart,
+          currentUserId: undefined,
+          client: undefined,
+          tasks: undefined,
+          refreshing: false,
+          search: "",
+          cart: global.cart,
         };
-    }
+      }
+      state={
+        drawerOpen: false,
+        drawerDisabled: false,
+      };
+    updateSearch = search => {
+        this.setState({ search });
+      }
+      closeDrawer = () => {
+        this._drawer.close()
+      };
+      openDrawer = () => {
+        this._drawer.open()
+      };
+      
     render() {
         const { navigation, data } = this.props;
         const { search } = this.state;
-
-        return (
+       return(
+          <Drawer
+          ref={(ref) => this._drawer = ref}
+          type="displace"
+          content={
+            <FilterScreen closeDrawer={this.closeDrawer} />
+          }
+          acceptDoubleTap
+          styles={{main: {paddingLeft:0}}}
+          onOpen={() => {
+            console.log('onopen')
+            this.setState({drawerOpen: true})
+          }}
+          onClose={() => {
+            console.log('onclose')
+            this.setState({drawerOpen: false})
+          }}
+          captureGestures={false}
+          tweenDuration={100}
+          panThreshold={0.08}
+          disabled={this.state.drawerDisabled}
+          openDrawerOffset={(viewport) => {
+            return 100
+          }}
+          closedDrawerOffset={() => 0}
+          panOpenMask={0.2}
+          negotiatePan
+          >
+            
             <View style={styles.container}>
-                <View style={styles.searchBarContainer}>
-                    <SearchBar
-                        placeholder="Type here..."
-                        round
-                        darkTheme
-                        onChangeText={this.updateSearch}
-                        value={search}
-                        style={styles.searchBar}
-                        onChangeText={search => this.setState({ search })}
-                        value={this.state.search}
-                        //onSubmitEditing={() => this.handleSubmit()}
-                    />
+                <View style={styles.searchbarcontainer}>
+                 <SearchBar containerStyle={{width:330}}
+            placeholder="Album, artist, lable..."
+            round
+            lightTheme
+            onChangeText={this.updateSearch}
+            value={search}
+            style={styles.input}
+            onChangeText={search => this.setState({ search })}
+            value={this.state.search}
+            onSubmitEditing={() => this.handleSubmit()}
+          />
+               <View style={{flex:1}}>
+               <Button
+               onPress={() => {this._drawer.open()}}
+              title="filter"
+               />
+                </View> 
+
+               
                 </View>
-                <ScrollView>
+                <View style={styles.searchresultscontainer}>
                     <SearchResultsList
-                        data={[
-                            { key: 'Devin' },
-                            { key: 'Jackson' },
-                            { key: 'James' },
-                            { key: 'Joel' },
-                            { key: 'John' },
-                            { key: 'Jillian' },
-                            { key: 'Jimmy' },
-                            { key: 'Julie' },
-                        ]} />
-                </ScrollView>
+              data={[
+                {key: 'Devin'},
+                {key: 'Jackson'},
+                {key: 'James'},
+                {key: 'Joel'},
+                {key: 'John'},
+                {key: 'Jillian'},
+                {key: 'Jimmy'},
+                {key: 'Julie'},
+              ]}
+                />
+                </View>
             </View>
+            </Drawer>
         );
     }
 }
 
 SearchScreen.navigationOptions = {
-    header: null,
-};
-
+    header:null,
+  };
+  
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: darkBlue,
+        flex:1,
+        paddingTop: 50,
+        paddingLeft:5,
+        paddingRight:5,
+        justifyContent:"flex-start",
+        backgroundColor: '#E5EEED',
     },
-    searchBarContainer: {
-        paddingTop: 25,
-    },
-    searchBar: {
-        backgroundColor: darkBlue,
-    },
+   
+    
+    searchbarcontainer:{
+        height:60,
+        flexDirection:'row',
 
+       
+    },
+    
+   searchresultscontainer:{
+       flex:1,
+       paddingTop:5
+
+   }
 })
