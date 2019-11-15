f7fd022058b5f3a570dbb2526363243aa3316ec3
HoneyPecanPie <44027796+HoneyPecanPie@users.noreply.github.com>
2019-11-14 22:48:24 -0600

    Update SearchScreen.js

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -5,65 +5,108 @@ import {
   View,
   TextInput,
   TouchableOpacity,
-  KeyboardAvoidingView
+  Button
 } from 'react-native';
-
+import SearchResultsList from '../components/SearchResultsList';
+import { SearchBar } from 'react-native-elements';
+import Drawer from 'react-native-drawer'
+import FilterScreen from './FilterScreen';
 export default class SearchScreen extends React.Component {
+  constructor(props) {
+        super(props);
+        this.state = {
+          currentUserId: undefined,
+          client: undefined,
+          tasks: undefined,
+          refreshing: false,
+          search: "",
+          cart: global.cart,
+        };
+      }
+      state={
+        drawerOpen: false,
+        drawerDisabled: false,
+      };
+    updateSearch = search => {
+        this.setState({ search });
+      }
+      closeDrawer = () => {
+        this._drawer.close()
+      };
+      openDrawer = () => {
+        this._drawer.open()
+      };
+      
     render() {
         const { navigation, data } = this.props;
-        return(
-            <KeyboardAvoidingView behavior="padding" style={styles.container}>
-                <View style={styles.headerContainer}>
-                    <Text style={styles.headerText}>Search by Album</Text>
-                </View>
-                <View style={styles.searchsectioncontainer}>
-                    <TextInput
-                    returnKeyType="next"
-                    placeholder="album name"
-                    placeholderTextColor="#636e72"
-                    onSubmitEditing={()=> this.albumInput.focus()}
-                    style={styles.input}
-                    />
-                </View>
-                    
-                <View style={styles.headerContainer}>
-                    <Text style={styles.headerText}>Search by Artist</Text>
-                </View>
-                <View style={styles.searchsectioncontainer}>
-                    <TextInput
-                    returnKeyType="next"
-                    placeholder="artist name"
-                    placeholderTextColor="#636e72"
-                    onSubmitEditing={()=>this.artistInput.focus()}
-                    ref={(input)=>this.albumInput=input}
-                    style={styles.input}
-                    />
+        const { search } = this.state;
+       return(
+          <Drawer
+          ref={(ref) => this._drawer = ref}
+          type="displace"
+          content={
+            <FilterScreen closeDrawer={this.closeDrawer} />
+          }
+          acceptDoubleTap
+          styles={{main: {paddingLeft:0}}}
+          onOpen={() => {
+            console.log('onopen')
+            this.setState({drawerOpen: true})
+          }}
+          onClose={() => {
+            console.log('onclose')
+            this.setState({drawerOpen: false})
+          }}
+          captureGestures={false}
+          tweenDuration={100}
+          panThreshold={0.08}
+          disabled={this.state.drawerDisabled}
+          openDrawerOffset={(viewport) => {
+            return 100
+          }}
+          closedDrawerOffset={() => 0}
+          panOpenMask={0.2}
+          negotiatePan
+          >
+            
+            <View style={styles.container}>
+                <View style={styles.searchbarcontainer}>
+                 <SearchBar containerStyle={{width:330}}
+            placeholder="Album, artist, lable..."
+            round
+            lightTheme
+            onChangeText={this.updateSearch}
+            value={search}
+            style={styles.input}
+            onChangeText={search => this.setState({ search })}
+            value={this.state.search}
+            onSubmitEditing={() => this.handleSubmit()}
+          />
+               <View style={{flex:1}}>
+               <Button
+               onPress={() => {this._drawer.open()}}
+              title="filter"
+               />
+                </View> 
 
+               
                 </View>
-                    
-                <View style={styles.headerContainer}>
-                    <Text style={styles.headerText}>Search by Label</Text>
+                <View style={styles.searchresultscontainer}>
+                    <SearchResultsList
+              data={[
+                {key: 'Devin'},
+                {key: 'Jackson'},
+                {key: 'James'},
+                {key: 'Joel'},
+                {key: 'John'},
+                {key: 'Jillian'},
+                {key: 'Jimmy'},
+                {key: 'Julie'},
+              ]}
+                />
                 </View>
-                <View style={styles.searchsectioncontainer}>
-                    <TextInput
-                    returnKeyType="search"
-                    placeholder="label name"
-                    placeholderTextColor="#636e72"
-                    ref={(input)=>this.artistInput=input}
-                    style={styles.input}
-                    />
-                </View>
-                
-                <TouchableOpacity 
-                style={styles.buttonContainer}
-                onPress={() => {
-                    /* TODO: Navigate to the Details route with params */
-                    navigation.navigate('SearchResults', {/* props go here */});
-                  }}>
-                    <Text style={styles.buttonText}>Search</Text>
-                </TouchableOpacity>
-                
-            </KeyboardAvoidingView>
+            </View>
+            </Drawer>
         );
     }
 }
@@ -76,26 +119,17 @@ const styles = StyleSheet.create({
     container: {
         flex:1,
         paddingTop: 50,
-        paddingLeft:10,
-        paddingRight:10,
+        paddingLeft:5,
+        paddingRight:5,
         justifyContent:"flex-start",
         backgroundColor: '#E5EEED',
     },
-    headerContainer: {
-        
-        height:30,
-    },
-    headerText: {
-        fontSize: 20,
-    },
-    input:{
-       flex:1,
-        backgroundColor:'#bdc3c7',
-        color:'#2d3436',
-        fontSize:20,
-    },
-    searchsectioncontainer:{
-        height:40,
+   
+    
+    searchbarcontainer:{
+        height:60,
+        flexDirection:'row',
+
        
     },
     buttonContainer:{
@@ -107,8 +141,18 @@ const styles = StyleSheet.create({
        justifyContent:"center",
        
     },
+    animatedBox: {
+        flex: 1,
+        backgroundColor: "#38C8EC",
+        padding: 10
+      },
    buttonText:{
        color:'#FFFFFF',
        fontSize:15
+   },
+   searchresultscontainer:{
+       flex:1,
+       paddingTop:5
+
    }
 })
